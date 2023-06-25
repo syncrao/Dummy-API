246b1a13b85306b9e0db2654b4b42d64be6812ec
Shahrukh Rao <88214192+raoshah@users.noreply.github.com>
2023-06-25 16:32:00 +0530

    Update app.py
    import os

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime, timedelta
 import psycopg2
 import pytz
+import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL').replace("postgres://", "postgresql://", 1)
