ac8ec11822fae8497b88f0142a574f3bf447120b
Shahrukh Rao <88214192+raoshah@users.noreply.github.com>
2023-06-25 16:17:47 +0530

    Update app.py
    d

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import psycopg2
 import pytz
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = ('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = (DATABASE_URL)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'sssrrrkkk'
 db = SQLAlchemy(app)
