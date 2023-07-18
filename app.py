be60b15503a4d88ee1e2185cefd3226611456ac8
Shahrukh Rao <88214192+raoshah@users.noreply.github.com>
2023-07-18 15:08:11 +0530

    Update database

--- app.py
+++ app.py
@@ -50,10 +50,10 @@ class Note(db.Model):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    # with app.app_context():
-    #     db.create_all()
     with app.app_context():
-        db.drop_all()
+        db.create_all()
+
+        # db.drop_all()
     if 'user_id' not in session:
         return redirect('/login')
 
