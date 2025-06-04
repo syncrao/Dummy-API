84df519bc9822902a9d51ccef34fcf3a5d8e11a7
raoshah <shahrukhrao1@gmail.com>
2025-06-04 15:17:11 +0530

    Jun 4

--- School_API/models.py
+++ School_API/models.py
@@ -260,7 +260,7 @@ class Athlete(models.Model):
 
 class Newuser(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    username = models.CharField(max_length=255)
+    username = models.CharField(max_length=255, unique=True)
     password = models.CharField(max_length=255)
 
     def __str__(self):
